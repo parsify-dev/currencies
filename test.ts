4fbbeab5857d546f550b3af2d7d0186bf12009e7
Antoni Kepinski <xxczaki@pm.me>
2020-03-06 21:19:59 +0100

    update tests

--- test.ts
+++ test.ts
@@ -2,14 +2,20 @@ import test from 'ava';
 
 import parsifyCurrencyPlugin from './src';
 
+const rates = {
+	GBP: 0.92,
+	EUR: 1.00,
+	USD: 1.12
+};
+
 test('general', async t => {
-    t.regex(await parsifyCurrencyPlugin()('12 usd to pln'), /PLN/);
-    t.regex(await parsifyCurrencyPlugin()('3.5 CHF to isk'), /ISK/);
-    t.regex(await parsifyCurrencyPlugin()('1 gBp TO EUR'), /EUR/);
+    t.regex(await parsifyCurrencyPlugin({base: 'EUR', rates})('12 usd to gbp'), /GBP/);
+    t.regex(await parsifyCurrencyPlugin({base: 'EUR', rates})('3.5 EUR in usd'), /USD/);
+    t.regex(await parsifyCurrencyPlugin({base: 'EUR', rates})('1 gBp TO EUR'), /EUR/);
 });
 
 test('if an error occurs, just output the expression', async t => {
-    t.is(await parsifyCurrencyPlugin()('12 foo to bar'), '12 foo to bar');
-    t.is(await parsifyCurrencyPlugin()('1+2'), '1+2');
-    t.is(await parsifyCurrencyPlugin()('12 usd'), '12 usd');
+    t.is(await parsifyCurrencyPlugin({base: 'EUR', rates})('12 foo to bar'), '12 foo to bar');
+    t.is(await parsifyCurrencyPlugin({base: 'EUR', rates})('1+2'), '1+2');
+    t.is(await parsifyCurrencyPlugin({base: 'EUR', rates})('12 usd'), '12 usd');
 });
